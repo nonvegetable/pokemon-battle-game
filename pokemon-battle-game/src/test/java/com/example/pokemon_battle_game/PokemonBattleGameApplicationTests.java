d0c4d2fcefb0a631b0c222e8bdeaa871f483e415
Vighnesh Palande <vpalande81203@outlook.com>
2025-09-07 01:54:14 +0530

    added spring-boot

---
+++ pokemon-battle-game/src/test/java/com/example/pokemon_battle_game/PokemonBattleGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pokemon_battle_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonBattleGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
